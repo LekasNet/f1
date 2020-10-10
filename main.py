fcddb695c411d07f33be2004f7a54c515e038a3c
LekasNet <58228148+LekasNet@users.noreply.github.com>
2020-10-10 15:51:11 +0300

    added grey form for work with angle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import numpy
+from matplotlib import pyplot as plt
+import numpy as np
 import cv2
+
 
 capture = cv2.VideoCapture(0)
 
@@ -7,7 +9,16 @@ capture = cv2.VideoCapture(0)
 while True:
     ret, img = capture.read()
 
-    cv2.imshow("stream", img)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    print(gray)
+    corners = cv2.goodFeaturesToTrack(gray, 27, 0.01, 10)
+    corners = np.int0(corners)
+
+    for i in corners:
+        x, y = i.ravel()
+        cv2.circle(img, (x, y), 3, 255, -1)
+    plt.imshow(img), plt.show()
+
 
 
     k = cv2.waitKey(30) & 0xFF
